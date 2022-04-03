9e01ce5a3bf87bfd80be958039f600e370e31a5f
smi73 <soren.mikkelsen@porsevej.dk>
2022-04-03 15:08:36 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Porsevej Engineering'
+copyright = '2022, Porsevej Engineering'
+author = 'Søren Mikkelsen'
 
 release = '0.1'
 version = '0.1.0'
